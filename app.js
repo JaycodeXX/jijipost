5b31a0b3f2ad197fcddba328e0180f2d297bffda
Onyeke Sunday Victor <diamondholdingslimited@gmail.com>
2023-03-14 15:25:31 +0100

    create index file

---
+++ app.js
@@ -0,0 +1,42 @@
+class Apartment{
+    constructor(title, propertyType, bedroom, bathroom, propertyAddress, estateName, propertySize,
+        condition, furnishing, features, price){
+            this.title = title;
+            this.propertyType = propertyType;
+            this.bedroom = bedroom;
+            this.bathroom = bathroom;
+            this.propertyAddress = propertyAddress;
+            this.estateName = estateName;
+            this.propertySize = propertySize;
+            this.condition = condition;
+            this.furnishing = furnishing;
+            this.features = features;
+            this.price = price;
+    }
+}
+
+class Car{
+    constructor(name, make, secondCondition, model, year, trim, body, drivetrain, 
+        engineSize, noOfCylinder, horsePower, color, seats, registered, usedType, fuelType,
+        control, price, negotiable){
+            this.name = name;
+            this.make = make;
+            this.secondCondition = secondCondition;
+            this.model = model;
+            this.year = year;
+            this.trim = trim;
+            this.body = body;
+            this.drivetrain = drivetrain;
+            this.engineSize = engineSize;
+            this.noOfCylinder = noOfCylinder;
+            this.horsePower = horsePower;
+            this.color = color;
+            this.seats = seats;
+            this.registered = registered;
+            this.usedType = usedType;
+            this.fuelType = fuelType;
+            this.control = control;
+            this.price = price;
+            this.negotiable = negotiable;
+        }
+}
